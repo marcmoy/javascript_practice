f2d5b2da3568bfd689a130141467a40d5172a8ab
Marc Moy <moymarc453@gmail.com>
2016-08-11 00:45:05 -0700

    finished exercises

--- oop_practice.js
+++ oop_practice.js
@@ -56,3 +56,35 @@ dumbo.trumpet();
 dumbo.grow();
 dumbo.addTrick("run");
 dumbo.play();
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function (elephant){
+  console.log(`${elephant.name} is trotting by!`);
+}
+
+Elephant.paradeHelper(micah);
+
+herd.forEach((elephant) => {
+  Elephant.paradeHelper(elephant);
+});
+
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs and bacon please.";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+};
+
+let bfastOrder = dinerBreakfast();
+bfastOrder("chocolate chip pancakes");
+bfastOrder("grits");
